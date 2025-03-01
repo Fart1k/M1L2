b394d10a2840c5913ce46e0f01d20e039ad05efd
Fart1k <seinartur@gmail.com>
2025-03-01 10:17:13 +0200

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -1,8 +1,24 @@
 import random
-symbols = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+engSymbols = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+ruSymbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890+-/*!&$#?=@"
+password = ""
 while True:
-    nums = int(input('Длина пароля: '))
     password = ""
-    for i in range(nums):
-        password += random.choice(symbols)
-    print(password)
+    nums = int(input('Длина пароля: '))
+    type = input("Русский, Английский, всё вместе? (1/2/3): ") 
+    if type == "1":
+        for i in range(nums):
+            password += random.choice(ruSymbols)
+        print(password)
+    elif type == "2":
+        for i in range(nums):
+            password += random.choice(engSymbols)
+        print(password)
+    elif type == "3":
+        for i in range(nums):
+            password += random.choice(engSymbols + ruSymbols)
+        print(password)
+    else:
+        print("Неверный ввод")
+
+    
